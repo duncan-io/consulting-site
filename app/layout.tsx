e3b410b726a18ed96fc0a25930d76a0e9fc80e31
Duncan McKee <dmckee95@gmail.com>
2026-04-23 21:31:53 -0400

    add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,20 @@ export default function RootLayout({
       lang="en"
       className={`${manrope.variable} ${inter.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+      {/* Google Tag Manager */}
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KWRM2KL6');</script>
+{/* End Google Tag Manager */}
+      </head>
       <body className="flex min-h-full flex-col bg-background text-foreground">
+      {/* Google Tag Manager (noscript) */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWRM2KL6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* End Google Tag Manager (noscript) */}
         <SiteHeader />
         <div className="flex-1">{children}</div>
         <SiteFooter />
